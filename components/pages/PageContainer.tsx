1f0489606a4495f57dee86f975bc9d0796f4e88e
chiknas <nkuknas@gmail.com>
2021-04-19 19:16:21 +0100

    app bar for server url/psw

--- components/pages/PageContainer.tsx
+++ components/pages/PageContainer.tsx
@@ -1,28 +1,59 @@
 import Head from "next/head";
-import React from "react";
+import React, { useState } from "react";
 import styles from "styles/Home.module.css";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { InputBase } from "@material-ui/core";
 
 export const PageContainer: React.FunctionComponent = ({ children }) => {
+  const [serverUrl, setServerUrl] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Swan Cloud</title>
-        <link rel="icon" href="/swan.svg" />
-      </Head>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">Swan Cloud</Typography>
+          <div className={styles.search}>
+            <InputBase
+              autoFocus={true}
+              placeholder="Server URL"
+              onChange={(event) => setServerUrl(event.target.value)}
+            />
+          </div>
+          <div className={styles.search}>
+            <InputBase
+              type="password"
+              placeholder="Password"
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
 
-      <main className={styles.main}>{children}</main>
+      <div className={styles.container}>
+        <Head>
+          <title>Swan Cloud</title>
+          <link rel="icon" href="/swan.svg" />
+        </Head>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://github.com/chiknas/SwanCloudServer"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/swan.svg" alt="Swan Logo" className={styles.logo} />
-          <b>SwanCloud</b>
-        </a>
-      </footer>
-    </div>
+        <main className={styles.main}>{children}</main>
+
+        <footer className={styles.footer}>
+          <a
+            href="https://github.com/chiknas/SwanCloudServer"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Typography variant="h6">
+              Powered by{" "}
+              <img src="/swan.svg" alt="Swan Logo" className={styles.logo} />{" "}
+              <b>SwanCloud</b>
+            </Typography>
+          </a>
+        </footer>
+      </div>
+    </>
   );
 };
